f16fe68bc487d3a42753e22be463f512dc4ce909
Soyeon Jo <rongha56@gmail.com>
2022-12-16 17:34:37 +0900

    chore: board.vo 내용 추가

--- younghae/src/main/java/com/kh/young/model/vo/Board.java
+++ younghae/src/main/java/com/kh/young/model/vo/Board.java
@@ -1,5 +1,15 @@
 package com.kh.young.model.vo;
 
-public class Board {
+import java.sql.Date;
 
+public class Board {
+	private int board_num;
+	private String board_title;
+	private String board_content;
+	private Date board_createDate;
+	private Date board_modifyDate;
+	private int board_view;
+	private String board_status;
+	private int user_num;
+	private int board_type;
 }
