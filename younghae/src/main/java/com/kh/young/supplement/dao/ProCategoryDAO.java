ad6762ef40d5f74d51e33a0e0b185d26bda9ff64
eclipse <sentiment916488@gmail.com>
2022-12-19 12:24:02 +0900

    fix : ProCategoryDAO.java
    ProcategoryDAO 관련 mybatis 오류 수정

--- younghae/src/main/java/com/kh/young/supplement/dao/ProCategoryDAO.java
+++ younghae/src/main/java/com/kh/young/supplement/dao/ProCategoryDAO.java
@@ -1,7 +1,9 @@
 package com.kh.young.supplement.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository("pcDAO")
 public class ProCategoryDAO {
 
 	public int getListCount(SqlSessionTemplate sqlSession) {
