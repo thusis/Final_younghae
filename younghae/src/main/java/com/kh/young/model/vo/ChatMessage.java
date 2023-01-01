3ad01e2c599f299816343ed5c9494adeeb6dc8c8
thusis <102267039+thusis@users.noreply.github.com>
2023-01-01 15:04:22 +0900

    fix : chat message 오타 수정

--- younghae/src/main/java/com/kh/young/model/vo/ChatMessage.java
+++ younghae/src/main/java/com/kh/young/model/vo/ChatMessage.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ChatMessage {
-	private int cathId;
+	private int chatId;
 	private String chatContent;
 	private Date chatTime;
 	private String chatRead;
